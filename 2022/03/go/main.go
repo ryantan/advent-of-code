c739b6222b801f1a93a8b2a259fc30af4a59c1de
Ryan Tan <ryan@redairship.com>
2022-12-04 00:22:21 +0800

    feat: day 3 in go

---
+++ 2022/03/go/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func priority(s string) int {
+	if len(s) == 0 {
+		return 0
+	}
+
+	charCode := int([]rune(s)[0])
+	if charCode > 96 {
+		return charCode - 96
+	}
+
+	return charCode - 38
+}
+
+func a() int {
+	f, err := os.Open("../input.txt")
+	//f, err := os.Open("../sample.txt")
+	if err != nil {
+		panic("Can't read input")
+	}
+
+	totalPriorities := 0
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		contents := []rune(scanner.Text())
+
+		var dup = ""
+
+		size := len(contents) / 2
+		hash := make(map[string]int, size)
+		for i := 0; i < size; i++ {
+			if hash[string(contents[i])] == 0 {
+				hash[string(contents[i])] += 1
+			}
+		}
+		for i := size; i < size*2; i++ {
+			if hash[string(contents[i])] == 1 {
+				dup = string(contents[i])
+				break
+			}
+		}
+
+		fmt.Printf("dup: %v\n", dup)
+		fmt.Printf("priority: %v\n", priority(dup))
+
+		totalPriorities += priority(dup)
+	}
+
+	fmt.Printf("totalPriorities: %d\n", totalPriorities)
+	return totalPriorities
+}
+
+func b() int {
+	f, err := os.Open("../input.txt")
+	//f, err := os.Open("../sample.txt")
+	if err != nil {
+		panic("Can't read input")
+	}
+
+	totalPriorities := 0
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		contents1 := []rune(scanner.Text())
+
+		scanner.Scan()
+		contents2 := []rune(scanner.Text())
+
+		scanner.Scan()
+		contents3 := []rune(scanner.Text())
+
+		var dup = ""
+
+		hash := make(map[string]int, len(contents1))
+
+		for i := 0; i < len(contents1); i++ {
+			if hash[string(contents1[i])] == 0 {
+				hash[string(contents1[i])] += 1
+			}
+		}
+		for i := 0; i < len(contents2); i++ {
+			if hash[string(contents2[i])] == 1 {
+				hash[string(contents2[i])] += 1
+			}
+		}
+		for i := 0; i < len(contents3); i++ {
+			if hash[string(contents3[i])] == 2 {
+				dup = string(contents3[i])
+				break
+			}
+		}
+
+		fmt.Printf("dup: %v\n", dup)
+		fmt.Printf("priority: %v\n", priority(dup))
+
+		totalPriorities += priority(dup)
+	}
+
+	fmt.Printf("totalPriorities: %d\n", totalPriorities)
+	return totalPriorities
+}
+
+func main() {
+	//a()
+	b()
+}
