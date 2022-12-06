3b61bcf065c8a3ca9d4f1fd1a5f8a24fd9156eac
Ryan Tan <ryan@redairship.com>
2022-12-06 15:20:39 +0800

    feat(day6): read by character clean up

--- 2022/06/go-2/main.go
+++ 2022/06/go-2/main.go
@@ -3,49 +3,38 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
+	"github/ryantan/advent-of-code/2022/common"
 )
 
 //var fileName = "../sample.txt"
 var fileName = "../input.txt"
 
 // Check if there are repeat chars in the block.
-func isDiff(block map[string]int) bool {
-	//fmt.Printf("block: %v\n", block)
+func isDiff(block map[string]int, blockSize int) bool {
+	ones := 0
 	for _, i := range block {
+		// Optional early return.
 		if i > 1 {
 			return false
 		}
+		ones += i
+		if ones == blockSize {
+			return true
+		}
 	}
-	return true
+	return false
 }
 
 func findSequenceOfDistinct(scanner *bufio.Scanner, scanner2 *bufio.Scanner, blockSize int) int {
 	buffer := make(map[string]int, blockSize)
-
-	// Check first block.
-	for i := 0; i < blockSize; i++ {
-		if !scanner.Scan() {
-			return 0
+	for i := 0; scanner.Scan(); i++ {
+		// Advance start of block scanner if we're past the first block.
+		if i >= blockSize {
+			scanner2.Scan()
+			buffer[scanner2.Text()]--
 		}
-		c := scanner.Text()
-		buffer[c]++
-	}
-	if isDiff(buffer) {
-		return 0
-	}
-
-	// Start of last blockSize chars.
-	for i := blockSize; scanner.Scan(); i++ {
-		scanner2.Scan()
-		startOfBlock := scanner2.Text()
-		latest := scanner.Text()
-		buffer[latest]++
-		buffer[startOfBlock]--
-		if buffer[startOfBlock] == 0 {
-			delete(buffer, startOfBlock)
-		}
-		if isDiff(buffer) {
+		buffer[scanner.Text()]++
+		if isDiff(buffer, blockSize) {
 			return i + 1
 		}
 	}
@@ -54,28 +43,16 @@ func findSequenceOfDistinct(scanner *bufio.Scanner, scanner2 *bufio.Scanner, blo
 	return 0
 }
 
-// getScanner opens a file and returns a bufio.Scanner.
-func getScanner(file string) *bufio.Scanner {
-	f, err := os.Open(file)
-	if err != nil {
-		panic("Can not read input " + file)
-	}
-
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanRunes)
-	return scanner
-}
-
 func a() {
-	scanner := getScanner(fileName)
-	scanner2 := getScanner(fileName)
+	scanner := common.GetCharScanner(fileName)
+	scanner2 := common.GetCharScanner(fileName)
 	processed := findSequenceOfDistinct(scanner, scanner2, 4)
 	fmt.Printf("Part1: %d\n", processed)
 }
 
 func b() {
-	scanner := getScanner(fileName)
-	scanner2 := getScanner(fileName)
+	scanner := common.GetCharScanner(fileName)
+	scanner2 := common.GetCharScanner(fileName)
 	processed := findSequenceOfDistinct(scanner, scanner2, 14)
 	fmt.Printf("Part2: %d\n", processed)
 }
