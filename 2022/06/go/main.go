946e5af8032aa62ee070f2b56623c66d34768250
Ryan Tan <ryan@redairship.com>
2022-12-06 13:35:34 +0800

    feat(day6): part 2 implementation

--- 2022/06/go/main.go
+++ 2022/06/go/main.go
@@ -6,12 +6,8 @@ import (
 	"os"
 )
 
-//func isDiff(c1 rune,c2 rune,c3 rune,c4 rune) bool {
-//	return c1 != c2 && c1 != c3 && c1 != c4 && (c2 != c3 && c2 != c4) && (c3 != c4)
-//}
-
 func isDiff(last4 map[rune]int) bool {
-	fmt.Printf("last4: %v\n", last4)
+	//fmt.Printf("last4: %v\n", last4)
 	for _, i := range last4 {
 		if i > 1 {
 			return false
@@ -20,32 +16,31 @@ func isDiff(last4 map[rune]int) bool {
 	return true
 }
 
-func findToken(s []rune) int {
-	if len(s) < 4 {
+func findSequenceOfDistinct(s []rune, count int) int {
+	if len(s) < count {
 		return 0
 	}
 
 	// Check first 4.
-	last4 := make(map[rune]int, 4)
-	last4[s[0]]++
-	last4[s[1]]++
-	last4[s[2]]++
-	last4[s[3]]++
-	fmt.Printf("first last4: %v\n", last4)
+	buffer := make(map[rune]int, count)
+	for i := 0; i < count; i++ {
+		buffer[s[i]]++
+	}
+	//fmt.Printf("first buffer: %v\n", buffer)
 
-	if isDiff(last4) {
+	if isDiff(buffer) {
 		return 0
 	}
 
-	// Start of last 4 chars.
+	// Start of last count chars.
 	x := 0
-	for i := 4; i < len(s); i++ {
-		last4[s[i]]++
-		last4[s[x]]--
-		if last4[s[x]] == 0 {
-			delete(last4, s[x])
+	for i := count; i < len(s); i++ {
+		buffer[s[i]]++
+		buffer[s[x]]--
+		if buffer[s[x]] == 0 {
+			delete(buffer, s[x])
 		}
-		if isDiff(last4) {
+		if isDiff(buffer) {
 			return i + 1
 		}
 		x++
@@ -65,13 +60,32 @@ func a() {
 
 	for scanner.Scan() {
 		l := []rune(scanner.Text())
-		processed := findToken(l)
+		processed := findSequenceOfDistinct(l, 4)
 		fmt.Printf("Part1: %d\n", processed)
 	}
 
 	fmt.Printf("Part1 done\n")
 }
 
+func b() {
+	//f, err := os.Open("../sample.txt")
+	f, err := os.Open("../input.txt")
+	if err != nil {
+		panic("Can't read input")
+	}
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		l := []rune(scanner.Text())
+		processed := findSequenceOfDistinct(l, 14)
+		fmt.Printf("Part2: %d\n", processed)
+	}
+
+	fmt.Printf("Part2 done\n")
+}
+
 func main() {
 	a()
+	b()
 }
