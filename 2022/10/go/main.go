efed50161d635cdab933a9609cbfb69b70946e82
Ryan Tan <ryan@redairship.com>
2022-12-11 00:15:16 +0800

    refactor(day10): clean up

--- 2022/10/go/main.go
+++ 2022/10/go/main.go
@@ -3,21 +3,15 @@ package main
 import (
 	"fmt"
 	"github/ryantan/advent-of-code/2022/common"
-	"strconv"
-	"strings"
 )
-
-//var fileName = "../sample.txt"
 
 var fileName = "../input.txt"
 
-func a() {
-	cycle := 0
-	nextSampleCycle := 20
-	totalSamplesValue := 0
-	value := 1
+func main() {
+	cycle, value, opValue := 0, 1, 0
+	nextSampleCycle, totalSamplesValue := 20, 0
 
-	takeSample := func() {
+	tick := func() {
 		x := cycle % 40
 		if x == value-1 || x == value || x == value+1 {
 			print("#")
@@ -29,36 +23,20 @@ func a() {
 		}
 
 		cycle++
-		//fmt.Printf("%d: %d\n", cycle, value)
 		if cycle == nextSampleCycle {
 			totalSamplesValue += nextSampleCycle * value
-			//fmt.Printf("=== %d: %d (%d)\n", cycle, value, nextSampleCycle*value)
 			nextSampleCycle += 40
 		}
 	}
 
 	scanner := common.GetLineScanner(fileName)
 	for scanner.Scan() {
-		l := strings.Split(scanner.Text(), " ")
-
-		if l[0] == "noop" {
-			takeSample()
-			continue
+		tick()
+		if _, err := fmt.Sscanf(scanner.Text(), "addx %d", &opValue); err == nil {
+			tick()
+			value += opValue
 		}
-
-		opValue, err := strconv.Atoi(l[1])
-		if err != nil {
-			panic("Could not parse input")
-		}
-
-		takeSample()
-		takeSample()
-		value += opValue
 	}
 
 	fmt.Printf("Part1: %d\n", totalSamplesValue)
-}
-
-func main() {
-	a()
 }
