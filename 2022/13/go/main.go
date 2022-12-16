4171a781516060334717a295b507d8360e98ed1a
Ryan Tan <ryan@redairship.com>
2022-12-16 13:47:50 +0800

    feat(day13): part 2 first implementation

--- 2022/13/go/main.go
+++ 2022/13/go/main.go
@@ -110,24 +110,31 @@ func (p *Packet) readNext() bool {
 
 // compare evaluates p as left packet and pRight as right packet and return true if order is correct.
 func (p *Packet) compare(pRight *Packet) bool {
+	// Reset
+	p.reset()
+	pRight.reset()
+
+	//fmt.Println(string(p.originalContent))
+	//fmt.Println(string(pRight.originalContent))
+
 	correctOrder := false
 	for i := 0; i < 10000; i++ {
 		hasNext1 := p.readNext()
 		hasNext2 := pRight.readNext()
 
-		fmt.Printf("Current elements: %d | %d\n", p.currentElement, pRight.currentElement)
+		//fmt.Printf("Current elements: %d | %d\n", p.currentElement, pRight.currentElement)
 
 		if !hasNext1 && !hasNext2 {
-			fmt.Printf("Both ran out\n")
+			//fmt.Printf("Both ran out\n")
 			break
 		} else if !hasNext1 && hasNext2 {
 			// Left ran out.
-			fmt.Printf("Left ran out\n")
+			//fmt.Printf("Left ran out\n")
 			correctOrder = true
 			break
 		} else if hasNext1 && !hasNext2 {
 			// Right ran out.
-			fmt.Printf("Right ran out\n")
+			//fmt.Printf("Right ran out\n")
 			break
 		}
 
@@ -136,17 +143,17 @@ func (p *Packet) compare(pRight *Packet) bool {
 			if p.currentElement == -1 && pRight.currentElement != -1 && pRight.currentElement != -2 {
 				// Left is a list, right is not a list (and not closing a list either.
 				// Convert right to list.
-				fmt.Printf("Convert right to list\n")
+				//fmt.Printf("Convert right to list\n")
 				pRight.convertToList()
 				p.readNext()
-				fmt.Printf("Current elements: %d | %d\n", p.currentElement, pRight.currentElement)
+				//fmt.Printf("Current elements: %d | %d\n", p.currentElement, pRight.currentElement)
 			} else if p.currentElement != -1 && pRight.currentElement == -1 && p.currentElement != -2 {
 				// Right is a list, left is not a list (and not closing a list either.
 				// Convert left to list.
-				fmt.Printf("Convert left to list\n")
+				//fmt.Printf("Convert left to list\n")
 				p.convertToList()
 				pRight.readNext()
-				fmt.Printf("Current elements: %d | %d\n", p.currentElement, pRight.currentElement)
+				//fmt.Printf("Current elements: %d | %d\n", p.currentElement, pRight.currentElement)
 			} else {
 				//doneAllConvertingWeNeedToDo = true
 				break
@@ -155,31 +162,28 @@ func (p *Packet) compare(pRight *Packet) bool {
 
 		if p.currentElement == -2 && pRight.currentElement != -2 {
 			// Left ends the list first.
-			fmt.Printf("Left list ran out\n")
+			//fmt.Printf("Left list ran out\n")
 			correctOrder = true
 			break
 		} else if p.currentElement != -2 && pRight.currentElement == -2 {
 			// Right ends the list first.
-			fmt.Printf("Right list ran out\n")
+			//fmt.Printf("Right list ran out\n")
 			break
 		} else if p.currentElement == -2 && pRight.currentElement == -2 {
 			// Both lists ended.
-			fmt.Printf("Both lists ended\n")
+			//fmt.Printf("Both lists ended\n")
 			continue
 		} else if p.currentElement < pRight.currentElement {
 			// Left is smaller.
-			fmt.Printf("Left is smaller\n")
+			//fmt.Printf("Left is smaller\n")
 			correctOrder = true
 			break
 		} else if p.currentElement > pRight.currentElement {
 			// Right is smaller.
-			fmt.Printf("Right is smaller\n")
+			//fmt.Printf("Right is smaller\n")
 			break
 		}
 	}
-	// Reset
-	p.reset()
-	pRight.reset()
 	return correctOrder
 }
 
@@ -201,19 +205,46 @@ func main() {
 		packets = append(packets, packet)
 	}
 
+	// Part 1.
 	sum := 0
 	for i := 0; i < len(packets)/2; i++ {
-		fmt.Printf("\n===== Pair %d:\n", i)
-		fmt.Println(string(packets[i*2].originalContent))
-		fmt.Println(string(packets[i*2+1].originalContent))
-
+		//fmt.Printf("\n===== Pair %d:\n", i)
 		if correctOrder := packets[i*2].compare(packets[i*2+1]); correctOrder {
-			fmt.Printf("Correct order!\n\n")
+			//fmt.Printf("Correct order!\n\n")
 			sum += i + 1
 		} else {
-			fmt.Printf("Wrong order!\n\n")
+			//fmt.Printf("Wrong order!\n\n")
 		}
 	}
 
 	fmt.Printf("Part 1: %d\n", sum)
+
+	// Part 2
+	// Add divider packets.
+	divider1 := &Packet{
+		originalContent: []rune("[[2]]"),
+	}
+	divider2 := &Packet{
+		originalContent: []rune("[[6]]"),
+	}
+	packets = append(packets, divider1, divider2)
+
+	for i := 0; i < len(packets)-1; i++ {
+		for j := 0; j < len(packets)-i-1; j++ {
+			if !packets[j].compare(packets[j+1]) {
+				packets[j], packets[j+1] = packets[j+1], packets[j]
+			}
+		}
+	}
+
+	pos1, pos2 := 0, 0
+	for i, packet := range packets {
+		if packet == divider1 {
+			pos1 = i + 1
+		} else if packet == divider2 {
+			pos2 = i + 1
+		}
+	}
+
+	fmt.Printf("Part 2: %d\n", pos1*pos2)
 }
