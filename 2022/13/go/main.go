38e8dc255aaea6d1705fc4e39eb989cea9d9f5ae
Ryan Tan <ryan@redairship.com>
2022-12-16 12:15:48 +0800

    feat(day13): part 1 first implementation

---
+++ 2022/13/go/main.go
@@ -0,0 +1,242 @@
+package main
+
+import (
+	"fmt"
+	"github/ryantan/advent-of-code/2022/common"
+	"strconv"
+)
+
+type Packet struct {
+	content         []rune
+	originalContent []rune
+	stack           []int
+	head            int
+	depth           int
+	currentElement  int
+}
+
+func (p *Packet) convertToList() {
+	// Pop.
+	originalElement := p.pop()
+
+	// Add list.
+	p.stack = append(p.stack, -1)
+
+	// Add back original element
+	p.stack = append(p.stack, originalElement)
+
+	// Update underlying string to close the list.
+	originalString := fmt.Sprintf("[%d]", originalElement)
+	aIndex := p.head - (len(originalString) - 2)
+	bIndex := p.head
+	b := p.content[bIndex:]
+
+	newContent := make([]rune, 0)
+	//fmt.Printf("p.content before part a: %s\n", string(p.content))
+	newContent = append(newContent, p.content[:aIndex]...)
+	newContent = append(newContent, []rune(originalString)...)
+	newContent = append(newContent, b...)
+	p.content = newContent
+	p.head++
+	//fmt.Printf("p.content after replace: %s\n", string(p.content))
+}
+
+func (p *Packet) pop() int {
+	lastElement := p.stack[len(p.stack)-1]
+	//fmt.Printf("p.stack before pop: %v\n", p.stack)
+	p.stack = p.stack[:len(p.stack)-1]
+	//fmt.Printf("p.stack after pop: %v\n", p.stack)
+	return lastElement
+}
+
+func (p *Packet) reset() {
+	p.content = p.originalContent
+	p.head = 0
+	p.stack = nil
+	p.depth = 0
+	p.currentElement = 0
+}
+func (p *Packet) readNext() bool {
+	if p.stack != nil {
+		// Non-first read
+		//p.head++
+		// Don't advance for first read.
+	}
+
+	if p.head >= len(p.content) {
+		// No more.
+		return false
+	}
+
+	c := p.content[p.head]
+	if c == '[' {
+		p.currentElement = -1
+		p.stack = append(p.stack, -1)
+		p.depth++
+		p.head++
+	} else if c == ']' {
+		p.currentElement = -2
+		p.pop()
+		p.depth--
+		p.head++
+		//// Attempt to read next comma if there is one.
+		//// TODO: Consider ignoring comma on next read instead of doing it at end of read.
+		//if p.head < len(p.content) {
+		//	nextChar := p.content[p.head]
+		//	if nextChar == ',' {
+		//		p.head++
+		//	}
+		//}
+	} else {
+		if c == ',' {
+			// Ignore
+			c = p.content[p.head]
+			p.head++
+		}
+
+		if c >= 48 && c <= 57 {
+			// Digit, read till next , or ] to terminate digit.
+			number := []rune{c}
+			p.head++
+
+			// Read all subsequent digits as long as they are not "," or "]"
+			for {
+				if p.head >= len(p.content) {
+					break
+				}
+				nextChar := p.content[p.head]
+				if nextChar == ']' {
+					break
+				} else if nextChar == ',' {
+					//p.head++
+					break
+				}
+				number = append(number, nextChar)
+				p.head++
+			}
+
+			element, err := strconv.Atoi(string(number))
+			if err != nil {
+				fmt.Printf("Could not parse %s\n", string(number))
+				panic("Could not parse number")
+			}
+			p.currentElement = element
+			p.stack = append(p.stack, element)
+		}
+	}
+
+	return true
+}
+
+// compare evaluates p as left packet and pRight as right packet and return true if order is correct.
+func (p *Packet) compare(pRight *Packet) bool {
+	correctOrder := false
+	for i := 0; i < 10000; i++ {
+		hasNext1 := p.readNext()
+		hasNext2 := pRight.readNext()
+
+		fmt.Printf("Current elements: %d | %d\n", p.currentElement, pRight.currentElement)
+
+		if !hasNext1 && !hasNext2 {
+			fmt.Printf("Both ran out\n")
+			break
+		} else if !hasNext1 && hasNext2 {
+			// Left ran out.
+			fmt.Printf("Left ran out\n")
+			correctOrder = true
+			break
+		} else if hasNext1 && !hasNext2 {
+			// Right ran out.
+			fmt.Printf("Right ran out\n")
+			break
+		}
+
+		//doneAllConvertingWeNeedToDo := false
+		for {
+			if p.currentElement == -1 && pRight.currentElement != -1 && pRight.currentElement != -2 {
+				// Left is a list, right is not a list (and not closing a list either.
+				// Convert right to list.
+				fmt.Printf("Convert right to list\n")
+				pRight.convertToList()
+				p.readNext()
+				fmt.Printf("Current elements: %d | %d\n", p.currentElement, pRight.currentElement)
+			} else if p.currentElement != -1 && pRight.currentElement == -1 && p.currentElement != -2 {
+				// Right is a list, left is not a list (and not closing a list either.
+				// Convert left to list.
+				fmt.Printf("Convert left to list\n")
+				p.convertToList()
+				pRight.readNext()
+				fmt.Printf("Current elements: %d | %d\n", p.currentElement, pRight.currentElement)
+			} else {
+				//doneAllConvertingWeNeedToDo = true
+				break
+			}
+		}
+
+		if p.currentElement == -2 && pRight.currentElement != -2 {
+			// Left ends the list first.
+			fmt.Printf("Left list ran out\n")
+			correctOrder = true
+			break
+		} else if p.currentElement != -2 && pRight.currentElement == -2 {
+			// Right ends the list first.
+			fmt.Printf("Right list ran out\n")
+			break
+		} else if p.currentElement == -2 && pRight.currentElement == -2 {
+			// Both lists ended.
+			fmt.Printf("Both lists ended\n")
+			continue
+		} else if p.currentElement < pRight.currentElement {
+			// Left is smaller.
+			fmt.Printf("Left is smaller\n")
+			correctOrder = true
+			break
+		} else if p.currentElement > pRight.currentElement {
+			// Right is smaller.
+			fmt.Printf("Right is smaller\n")
+			break
+		}
+	}
+	// Reset
+	p.reset()
+	pRight.reset()
+	return correctOrder
+}
+
+func main() {
+	//scanner := common.GetLineScanner("../sample.txt")
+	//scanner := common.GetLineScanner("../sample2.txt")
+	scanner := common.GetLineScanner("../input.txt")
+
+	currentPairIndex := 1
+	sum := 0
+	for scanner.Scan() {
+		fmt.Printf("\n===== Pair %d:\n", currentPairIndex)
+		l1 := []rune(scanner.Text())
+		scanner.Scan()
+		l2 := []rune(scanner.Text())
+
+		packet1 := &Packet{
+			originalContent: l1,
+			content:         l1,
+		}
+		packet2 := &Packet{
+			originalContent: l2,
+			content:         l2,
+		}
+		//fmt.Println(string(l1))
+		//fmt.Println(string(l2))
+
+		if correctOrder := packet1.compare(packet2); correctOrder {
+			fmt.Printf("Correct order!\n\n")
+			sum += currentPairIndex
+		} else {
+			fmt.Printf("Wrong order!\n\n")
+		}
+		currentPairIndex++
+		scanner.Scan()
+	}
+
+	fmt.Printf("Part 1: %d\n", sum)
+
+}
