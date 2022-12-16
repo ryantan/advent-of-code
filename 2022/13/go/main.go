137938ebb4d19c4bfecbab581b0e04c2e978d29d
Ryan Tan <ryan@redairship.com>
2022-12-16 14:33:00 +0800

    feat(day13): clean up

--- 2022/13/go/main.go
+++ 2022/13/go/main.go
@@ -9,51 +9,39 @@ import (
 type Packet struct {
 	content         []rune
 	originalContent []rune
-	stack           []int
 	head            int
-	depth           int
-	currentElement  int
+	currentElement  int // -1 for '[', -2 for ']'
 }
 
+func NewPacket(content string) *Packet {
+	p := Packet{
+		originalContent: []rune(content),
+	}
+	return &p
+}
+
+// convertToList converts an int to a list.
 func (p *Packet) convertToList() {
-	// Pop.
-	originalElement := p.pop()
-
-	// Add list.
-	p.stack = append(p.stack, -1)
-
-	// Add back original element
-	p.stack = append(p.stack, originalElement)
-
 	// Update underlying string to close the list.
-	originalString := fmt.Sprintf("[%d]", originalElement)
+	originalString := fmt.Sprintf("[%d]", p.currentElement)
 	aIndex := p.head - (len(originalString) - 2)
-	bIndex := p.head
-	b := p.content[bIndex:]
 
-	newContent := make([]rune, 0)
 	//fmt.Printf("p.content before part a: %s\n", string(p.content))
-	newContent = append(newContent, p.content[:aIndex]...)
+	newContent := append([]rune{}, p.content[:aIndex]...)
 	newContent = append(newContent, []rune(originalString)...)
-	newContent = append(newContent, b...)
+	newContent = append(newContent, p.content[p.head:]...)
 	p.content = newContent
-	p.head++
+	p.head++ // Preserve position after adding the new '['.
 	//fmt.Printf("p.content after replace: %s\n", string(p.content))
-}
-
-func (p *Packet) pop() int {
-	lastElement := p.stack[len(p.stack)-1]
-	p.stack = p.stack[:len(p.stack)-1]
-	return lastElement
 }
 
 func (p *Packet) reset() {
 	p.content = p.originalContent
 	p.head = 0
-	p.stack = nil
-	p.depth = 0
 	p.currentElement = 0
 }
+
+// readNext reads the next element, either a '[', ']' or int. Returns false if end of content.
 func (p *Packet) readNext() bool {
 	if p.head >= len(p.content) {
 		// No more.
@@ -63,13 +51,9 @@ func (p *Packet) readNext() bool {
 	c := p.content[p.head]
 	if c == '[' {
 		p.currentElement = -1
-		p.stack = append(p.stack, -1)
-		p.depth++
 		p.head++
 	} else if c == ']' {
 		p.currentElement = -2
-		p.pop()
-		p.depth--
 		p.head++
 	} else {
 		if c == ',' {
@@ -79,21 +63,14 @@ func (p *Packet) readNext() bool {
 		}
 
 		if c >= 48 && c <= 57 {
-			// Digit, read till next , or ] to terminate digit.
-			number := []rune{c}
-			p.head++
-
-			// Read all subsequent digits as long as they are not "," or "]"
-			for {
-				if p.head >= len(p.content) {
-					break
-				}
+			// Digit, read all subsequent digits as long as they are not "," or "]"
+			var number []rune
+			for ; p.head < len(p.content); p.head++ {
 				nextChar := p.content[p.head]
 				if nextChar == ']' || nextChar == ',' {
 					break
 				}
 				number = append(number, nextChar)
-				p.head++
 			}
 
 			element, err := strconv.Atoi(string(number))
@@ -101,7 +78,6 @@ func (p *Packet) readNext() bool {
 				panic("Could not parse number")
 			}
 			p.currentElement = element
-			p.stack = append(p.stack, element)
 		}
 	}
 
@@ -117,45 +93,36 @@ func (p *Packet) compare(pRight *Packet) bool {
 	//fmt.Println(string(p.originalContent))
 	//fmt.Println(string(pRight.originalContent))
 
-	correctOrder := false
-	for i := 0; i < 10000; i++ {
+	for {
 		hasNext1 := p.readNext()
 		hasNext2 := pRight.readNext()
-
-		//fmt.Printf("Current elements: %d | %d\n", p.currentElement, pRight.currentElement)
-
 		if !hasNext1 && !hasNext2 {
 			//fmt.Printf("Both ran out\n")
-			break
+			return false
 		} else if !hasNext1 && hasNext2 {
 			// Left ran out.
 			//fmt.Printf("Left ran out\n")
-			correctOrder = true
-			break
+			return true
 		} else if hasNext1 && !hasNext2 {
 			// Right ran out.
 			//fmt.Printf("Right ran out\n")
-			break
+			return false
 		}
 
-		//doneAllConvertingWeNeedToDo := false
+		// Repeatedly convert to list till we are ready to compare ints.
 		for {
-			if p.currentElement == -1 && pRight.currentElement != -1 && pRight.currentElement != -2 {
-				// Left is a list, right is not a list (and not closing a list either.
-				// Convert right to list.
+			//fmt.Printf("Current elements: %d | %d\n", p.currentElement, pRight.currentElement)
+			if p.currentElement == -1 && pRight.currentElement >= 0 {
+				// Left is a list, right is an int. Convert right to list.
 				//fmt.Printf("Convert right to list\n")
 				pRight.convertToList()
 				p.readNext()
-				//fmt.Printf("Current elements: %d | %d\n", p.currentElement, pRight.currentElement)
-			} else if p.currentElement != -1 && pRight.currentElement == -1 && p.currentElement != -2 {
-				// Right is a list, left is not a list (and not closing a list either.
-				// Convert left to list.
+			} else if p.currentElement >= 0 && pRight.currentElement == -1 {
+				// Right is a list, left is an int. Convert left to list.
 				//fmt.Printf("Convert left to list\n")
 				p.convertToList()
 				pRight.readNext()
-				//fmt.Printf("Current elements: %d | %d\n", p.currentElement, pRight.currentElement)
 			} else {
-				//doneAllConvertingWeNeedToDo = true
 				break
 			}
 		}
@@ -163,72 +130,53 @@ func (p *Packet) compare(pRight *Packet) bool {
 		if p.currentElement == -2 && pRight.currentElement != -2 {
 			// Left ends the list first.
 			//fmt.Printf("Left list ran out\n")
-			correctOrder = true
-			break
+			return true
 		} else if p.currentElement != -2 && pRight.currentElement == -2 {
 			// Right ends the list first.
 			//fmt.Printf("Right list ran out\n")
-			break
-		} else if p.currentElement == -2 && pRight.currentElement == -2 {
-			// Both lists ended.
-			//fmt.Printf("Both lists ended\n")
-			continue
+			return false
 		} else if p.currentElement < pRight.currentElement {
 			// Left is smaller.
 			//fmt.Printf("Left is smaller\n")
-			correctOrder = true
-			break
+			return true
 		} else if p.currentElement > pRight.currentElement {
 			// Right is smaller.
 			//fmt.Printf("Right is smaller\n")
-			break
+			return false
 		}
 	}
-	return correctOrder
 }
 
 func main() {
 	//scanner := common.GetLineScanner("../sample.txt")
-	//scanner := common.GetLineScanner("../sample2.txt")
 	scanner := common.GetLineScanner("../input.txt")
 
+	// Read all packets.
 	packets := make([]*Packet, 0)
 	for scanner.Scan() {
-		l1 := []rune(scanner.Text())
-		if len(l1) == 0 {
+		l := scanner.Text()
+		if len(l) == 0 {
 			continue
 		}
-		packet := &Packet{
-			originalContent: l1,
-			content:         l1,
-		}
-		packets = append(packets, packet)
+		packets = append(packets, NewPacket(l))
 	}
 
 	// Part 1.
 	sum := 0
 	for i := 0; i < len(packets)/2; i++ {
-		//fmt.Printf("\n===== Pair %d:\n", i)
-		if correctOrder := packets[i*2].compare(packets[i*2+1]); correctOrder {
-			//fmt.Printf("Correct order!\n\n")
+		if packets[i*2].compare(packets[i*2+1]) {
 			sum += i + 1
-		} else {
-			//fmt.Printf("Wrong order!\n\n")
 		}
 	}
-
 	fmt.Printf("Part 1: %d\n", sum)
 
 	// Part 2
 	// Add divider packets.
-	divider1 := &Packet{
-		originalContent: []rune("[[2]]"),
-	}
-	divider2 := &Packet{
-		originalContent: []rune("[[6]]"),
-	}
+	divider1 := NewPacket("[[2]]")
+	divider2 := NewPacket("[[6]]")
 	packets = append(packets, divider1, divider2)
 
+	// Bubble sort.
 	for i := 0; i < len(packets)-1; i++ {
 		for j := 0; j < len(packets)-i-1; j++ {
 			if !packets[j].compare(packets[j+1]) {
@@ -237,14 +185,14 @@ func main() {
 		}
 	}
 
-	pos1, pos2 := 0, 0
+	// Look for pos.
+	// We can keep track of positions during the sort, but at higher cost.
+	decoderKey := 1
 	for i, packet := range packets {
-		if packet == divider1 {
-			pos1 = i + 1
-		} else if packet == divider2 {
-			pos2 = i + 1
+		if packet == divider1 || packet == divider2 {
+			decoderKey *= i + 1
 		}
 	}
 
-	fmt.Printf("Part 2: %d\n", pos1*pos2)
+	fmt.Printf("Part 2: %d\n", decoderKey)
 }
